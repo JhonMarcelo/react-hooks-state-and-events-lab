d400bb25ff40f532d0b208279be46452286ada5f
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-06-03 18:20:22 -0400

    Complete React State Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleButtonCart(e) {
+    setInCart(!inCart);
+  }
+
+  const cartStatus = inCart ? "in-cart" : "";
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleButtonCart} className="add">
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
